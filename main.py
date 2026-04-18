33aeb6dcc53181de2cb9b5334d0c425cc6dd4f61
juanigremes <juanigremes@gmail.com>
2026-04-18 16:24:33 -0300

    agregue input para la ia. tambien una clculadora

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if api_key == None:
-    raise RuntimeError("api_key wasn't found")
-
-# Gemini Client Instance
-client = genai.Client(api_key=api_key)
-
-# Interacting with Gemini
-response = client.models.generate_content(model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-print(response.text)
+import argparse
+from google.genai import types
 
 
 
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("api_key wasn't found")
+
+    # Parser
+    parser = argparse.ArgumentParser(description="Send cmd to my Agent")
+    parser.add_argument("user_prompt", type=str, help="prompt help")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    # Messages
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    # Gemini Client Instance
+    client = genai.Client(api_key=api_key)
+    
+    response = interact_with_gemini(client, messages)
+
+    if args.verbose:
+        print("User prompt: ",args.user_prompt)
+        print("Prompt tokens: ",response.usage_metadata.prompt_token_count)
+        print("Response tokens: ",response.usage_metadata.prompt_token_count) 
+
+    print(response.text)
+
+
+
+
+def interact_with_gemini(client, messages):
+    response = client.models.generate_content(model='gemini-2.5-flash', contents=messages)
+    if response.usage_metadata == None:
+        raise RuntimeError("response's usage metadata is None, failed API request")
+    return response
+
+
+# call to main() func
+main()
