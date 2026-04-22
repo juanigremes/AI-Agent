2fd0f2e4cd8141b58b9077e2b1329bda4d4463ef
juanigremes <juanigremes@gmail.com>
2026-04-22 17:23:33 -0300

    casi terminado

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 import argparse
@@ -12,6 +13,9 @@ def main():
     if api_key == None:
         raise RuntimeError("api_key wasn't found")
 
+    # Gemini Client Instance
+    client = genai.Client(api_key=api_key)
+    
     # Parser
     parser = argparse.ArgumentParser(description="Send cmd to my Agent")
     parser.add_argument("user_prompt", type=str, help="prompt help")
@@ -21,45 +25,53 @@ def main():
     # Messages
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    # Gemini Client Instance
-    client = genai.Client(api_key=api_key)
-    
-    response = interact_with_gemini(client, messages)
 
-    if args.verbose:
-        print("User prompt: ",args.user_prompt)
-        print("Prompt tokens: ",response.usage_metadata.prompt_token_count)
-        print("Response tokens: ",response.usage_metadata.prompt_token_count) 
-
-    function_results = []
-
-    if response.function_calls != None:
-        for function_call in response.function_calls:
-            #print(f"Calling function: {function_call.name}({function_call.args})")
-
-            if args.verbose:
-                f_call_res = call_function(function_call, verbose=True)
-            else:
-                f_call_res = call_function(function_call)
-
-            parts = f_call_res.parts
-            if parts == []:
-                raise Exception("types.Content object \"f_call_res\" has an empty .parts list")
-            parts_function_response = parts[0].function_response
-            if parts_function_response == None:
-                raise Exception(".parts[0].function_response is None")
-            parts_function_response_response = parts_function_response.response
-            if parts_function_response_response == None:
-                raise Exception("response is None")
-
-            function_results.append(parts[0])
-
-            if args.verbose :
-                print(f"-> {f_call_res.parts[0].function_response.response}")
-    else:
-        print(response.text)
+    # Loop
+    for _ in range(20):
+        response = interact_with_gemini(client, messages)
 
 
+        if args.verbose:
+            print("User prompt: ",args.user_prompt)
+            print("Prompt tokens: ",response.usage_metadata.prompt_token_count)
+            print("Response tokens: ",response.usage_metadata.prompt_token_count) 
+
+        function_results = []
+
+        if response.function_calls != None:
+            for function_call in response.function_calls:
+                #print(f"Calling function: {function_call.name}({function_call.args})")
+
+                if args.verbose:
+                    f_call_res = call_function(function_call, verbose=True)
+                else:
+                    f_call_res = call_function(function_call)
+
+                parts = f_call_res.parts
+                if parts == []:
+                    raise Exception("types.Content object \"f_call_res\" has an empty .parts list")
+                parts_function_response = parts[0].function_response
+                if parts_function_response == None:
+                    raise Exception(".parts[0].function_response is None")
+                parts_function_response_response = parts_function_response.response
+                if parts_function_response_response == None:
+                    raise Exception("response is None")
+
+                function_results.append(parts[0])
+
+                if args.verbose :
+                    print(f"-> {f_call_res.parts[0].function_response.response}")
+        else:
+            print(response.text)
+            return
+        
+ 
+        if response.candidates != None:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+                messages.append(types.Content(role="user", parts=function_results))       
+    print("Reached maximum number of iterations and the agent hasn't produced a final response")
+    sys.exit(1)
 
 def interact_with_gemini(client, messages):
     response = client.models.generate_content(
